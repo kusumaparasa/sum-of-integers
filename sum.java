f4dd2fec9b596fc7831600a5a3aa99315cbbe5d1
kusumaparasa <kusumap1033@gmail.com>
2022-02-15 15:48:42 +0530

    program of sum of integers

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+class sum
+{
+	public static void main(string args[])
+	{
+		scanner sc=new scanner(system.in);
+		int x,y,z;
+		system.out.println("enter x,y values");
+		x=sc.nextInt();
+		y=sc.nextInt();
+		z=x+y;
+		system.out.println("z value="+z);
+	}
+}
